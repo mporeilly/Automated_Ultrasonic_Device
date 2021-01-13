7c229ede7a07517165d75d9c82a84bf5a7630761
mporeilly <matthew.oreilly@ucalgary.ca>
2021-01-13 09:38:51 -0700

    Building Scan Function Setup

--- scan_function.py
+++ scan_function.py
@@ -1,7 +1,11 @@
 def scan_func(width,length,unit):
     # will calculate number of inputs required for the stepper motors 
     # this should also be where the scope data is collected 
-
+    if unit = 1:
+        unit_symbol = 'in'
+    else:
+        unit_symbol = 'mm'
+    print('width of the scan is ' + width + unit_symbol + ', length of the scan is '+ length + unit unit_symbol)
 
 
     # should return data of the scan
--- serial_data_collector.py
+++
@@ -1,3 +0,0 @@
-#https://www.youtube.com/watch?v=bKHLTn_nXUM&ab_channel=Kevin
-# following the above video a script will be written to collect the serial data sent over the RS232 connection off the back of the scope
-#https://pimylifeup.com/raspberry-pi-on-screen-keyboard/
