e06e0662d52d6b376f7dacb2cf179a0d9eb749ea
chasecasavant <72833640+chasecasavant@users.noreply.github.com>
2021-02-04 12:26:59 -0700

    motor code update

--- WheelStepperFunction.py
+++ WheelStepperFunction.py
@@ -3,16 +3,18 @@ def movewheels(radius, degrees, distance, direction):
     import time
     import RPi.GPIO as GPIO
 
-    if direction == "forward":
-        direction = int(1)  # clockwise/forward (direction pin high)
+    # if direction == "forward":
+    #     direction = int(1)  # clockwise/forward (direction pin high)
+    #
+    # else:
+    #     direction = int(0)  # clockwise/reverse (direction pin low)
 
-    else:
-        direction = int(0)  # clockwise/reverse (direction pin low)
+    # CHANGE PINS STEP AND DIR BACK
 
     step_angle = degrees/2
     m_pins = (25, 8, 7)  # M-pins of driver set to GPIO pins 25 8 and 7 on pi
-    step = 20  # step pin of driver set to GPIO 20 on pi
-    dir = 21  # dir pin of driver set to GPIO 21 on pi
+    step = 23  # step pin of driver set to GPIO 20 on pi
+    dir = 24  # dir pin of driver set to GPIO 21 on pi
     half = (1, 0, 0)  # sequence of m-pins activation for half stepping (M0 high, M1 and M2 low)
 
     # Note that may be able to just pull the M-pins high and low by connecting M0 to 3.3v rail, M1 and M2 to ground
@@ -21,10 +23,10 @@ def movewheels(radius, degrees, distance, direction):
     GPIO.setmode(GPIO.BCM)  # Sets pin naming convention
     GPIO.setup(dir, GPIO.OUT)  # setting the direction, step, and resolution pins as outputs
     GPIO.setup(step, GPIO.OUT)
-    GPIO.setup(m_pins, GPIO.OUT)
+    # GPIO.setup(m_pins, GPIO.OUT)
 
     GPIO.output(dir, direction)  # sets dir pin high or low based on direction that it is passed (forward/reverse)
-    GPIO.output(m_pins, half)  # sets half step mode (M0 high, M1 and M2 low)
+    # GPIO.output(m_pins, half)  # sets half step mode (M0 high, M1 and M2 low)
 
     counter = 0
     maximum = int(distance / (step_angle * ((2 * math.pi) / 360) * radius))  # determines half steps to travel distance
@@ -35,5 +37,5 @@ def movewheels(radius, degrees, distance, direction):
         GPIO.output(step, GPIO.LOW)  # step pin set low
         time.sleep(0.01)  # delay between pulses (test different values)
 
-    go = 1
-    return go  # once wheels have moved desired distance, the return value initiates the next function call in main file
+    #  go = 1
+    #  return go  # once wheels have moved desired distance, the return value initiates the next function call in main
--- scan_controller.py
+++ scan_controller.py
@@ -1,6 +1,22 @@
 def scan_control(width, length, gate_start, gate_width, unit, operation_flag):
 
-    # if statement to make sure the values fit within the 
+    # if statement to make sure the values fit within the
+    import math
+    from WheelStepperFunction import movewheels  # importing created functions from external files
+    from TransducerStepperFunction import movescanner
+    if unit == 2:
+        radius = 1.5  # wheel radius in inches
+        belt = 1  # temporary belt radius in inches
+        yincrement = 0.75  # y increment in inches
+    if unit == 1:
+        radius = 38.1  # wheel radius in mm
+        belt = 25.4  # temporary belt radius in mm
+        yincrement = 19.05  # y increment in mm
+
+    scanPath = width  # length of scanner path
+    degrees = 1.8/2  # degrees per step (for full stepping, half stepping is utilized in the move functions)
+    stepincrement = int(yincrement / (degrees * ((2 * math.pi) / 360) * radius))  # number of steps in y increment
+
     if unit != 1 or unit != 2:
         return
 
@@ -12,14 +28,8 @@ def scan_control(width, length, gate_start, gate_width, unit, operation_flag):
 
     def measurement_to_impulse(length,unit):
         #returns the number of impulses required to drive the
-        if unit == 1: # this is the milimeter case
-            print('milimeter calculation function activated')
-            return impulse_number
-            
-        if unit == 2: # this is the milimeter case
-            print('inch calculation function activated')
-            return impulse_number
-
+        impulse_number = int(length / (degrees * ((2 * math.pi) / 360) * radius)) # steps per length
+        return impulse_number
 
     def voltage_collector():
         print('voltage collector activated')
@@ -34,19 +44,34 @@ def scan_control(width, length, gate_start, gate_width, unit, operation_flag):
 
     while operation_flag == 1: # this is for the emergency stop to be wired to
 
+        length_impulses = measurement_to_impulse(length, unit) / stepincrement # number of forward increments in grid
+        width_impulses = measurement_to_impulse(length,unit)  # number of steps per probe sweep
+        xdirection = 1  # starting direction of probe set to left->right
+        ydirection = 1  # starting direction of wheels set to forward
+        ximpulses = 0 # variable for counting impulses to determine direction (odd impulses = left->right, vise versa)
+        yimpulses = 0 # variable for counting impulses to determine direction (odd impulses = forward, vise versa)
 
-        length_impulses = measurement_to_impulse(length,unit)
-        width_impulses = measurement_to_impulse(length,unit)
+        for y_movement in length_impulses:
+            movewheels(radius, degrees, yincrement, ydirection)
+            if y_movement == length_impulses:  # when length is reached, reverse wheel direction
+                ydirection = 0
+            # if gpiopins is high:
+            #     operation_flag = 0    # this will stop the machine from moving when the emergency stop is activated
+            voltage = voltage_collector()
+            thickness = interpolation_func(voltage, gate_start, gate_width)
+            # https://www.youtube.com/watch?v=Ercd-Ip5PfQ&ab_channel=CoreySchafer
 
-        for x_movement in length_impulses:
-                
-            print(x_movement)
+            if y_movement % 2 != 0:  # this reverses x direction after each probe sweep
+                xdirection = 0
+            else:
+                xdirection = 1
+            print(y_movement)
 
-            for y_movement in width_impulses:              
-                # if gpiopins is high:
-                #     operation_flag = 0    # this will stop the machine from moving when the emegency stop is activated
-                voltage = voltage_collector()
-                thickness = interpolation_func(voltage, gate_start, gate_width)
-                # https://www.youtube.com/watch?v=Ercd-Ip5PfQ&ab_channel=CoreySchafer
-                print(y_movement)
+           for x_movement in width_impulses:
+               yimpulses +=1
+               movescanner(belt, degrees, width_impulses, xdirection)
+               ximpulses +=1
+               print(x_movement)
+
+
 
