0d4302622a607db2cbc05dd1cbd0c71dad7060ba
mporeilly <matthew.oreilly@ucalgary.ca>
2021-03-18 11:24:32 -0600

    Update atodtester.py

--- atodtester.py
+++ atodtester.py
@@ -11,7 +11,7 @@ cs = digitalio.DigitalInOut(board.D5)
 mcp = MCP.MCP3008(spi, cs)
 channel = AnalogIn(mcp, MCP.P0)
 
-
+GPIO.setmode(GPIO.BCM)
 while True:
 
     print('Raw ADC Value: ', channel.value)
