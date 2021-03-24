5e3353adba219cc5d6360b1c43777796528eddc8
mporeilly <matthew.oreilly@ucalgary.ca>
2021-03-24 14:52:00 -0600

    Update arduinocontrol.py

--- arduinocontrol.py
+++ arduinocontrol.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3 
+#https://www.youtube.com/watch?v=3QSsnnbJYFc&ab_channel=ElitheComputerGuy
 import serial
-import time
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5) 
-# read from Arduino
-input = ser.read()
-print("Read input " + input.decode("utf-8") + " from Arduino")
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
+
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
