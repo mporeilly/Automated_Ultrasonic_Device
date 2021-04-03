a795e449392ce419ee09113b0a25b6f0b897dab5
mporeilly <matthew.oreilly@ucalgary.ca>
2021-04-03 12:22:22 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # chmod 775 the setup.sh file 
 # run   sudo ./setup.sh
 
-python3 -m venv capstone_venv       #sets up virtual enviornment
-source capstone_venv/bin/active
-pip install -r requirements.txt
-python main.py
+pip install pandas
+pip install tkinter
+pip install matplotlib
+pip install numpy
